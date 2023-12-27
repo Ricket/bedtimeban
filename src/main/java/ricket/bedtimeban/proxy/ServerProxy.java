9211a972a61dffc1ca6d767d77abb23122ab2889
Richard Carter <richard@richardcarter.org>
2023-12-27 01:06:51 -0500

    Add logging for lightning, death, and despawn events
    In an attempt to determine the source of random animal mob deaths.

--- src/main/java/ricket/bedtimeban/proxy/ServerProxy.java
+++ src/main/java/ricket/bedtimeban/proxy/ServerProxy.java
@@ -3,11 +3,20 @@ package ricket.bedtimeban.proxy;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent.AllowDespawn;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
@@ -64,5 +73,58 @@ public class ServerProxy implements IProxy {
 
     @SubscribeEvent
     public void playerLoggedOut(PlayerLoggedOutEvent event) {
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void entityStruckByLightning(EntityStruckByLightningEvent event) {
+        if (event.isCanceled()) {
+            return;
+        }
+        Entity entity = event.getEntity();
+        if (entity == null) {
+            BedtimeBanMod.logger.warn("EntityStruckByLightningEvent with no entity");
+            return;
+        }
+        BedtimeBanMod.logger.info("Entity struck by lightning: " + entity);
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void entityDeath(LivingDeathEvent event) {
+        if (event.isCanceled()) {
+            return;
+        }
+        EntityLivingBase entity = event.getEntityLiving();
+        if (entity == null) {
+            BedtimeBanMod.logger.warn("LivingDeathEvent with no entity");
+            return;
+        }
+        DamageSource source = event.getSource();
+        BedtimeBanMod.logger.info("Entity died: " + entity + " ; message: " + damageSourceToString(source, entity));
+    }
+
+    private String damageSourceToString(DamageSource damageSource, EntityLivingBase entity) {
+        if (damageSource == null) {
+            return "null";
+        }
+        ITextComponent deathMessage = damageSource.getDeathMessage(entity);
+        return deathMessage.getUnformattedText();
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void logForcedDespawns(LivingSpawnEvent parentEvent) {
+        if (!(parentEvent instanceof AllowDespawn) || parentEvent.isCanceled()) {
+            return;
+        }
+        AllowDespawn event = (AllowDespawn) parentEvent;
+        Result result = event.getResult();
+        if (result == Result.ALLOW) {
+            // ALLOW will force the mob to despawn
+            EntityLivingBase entity = event.getEntityLiving();
+            if (entity == null) {
+                BedtimeBanMod.logger.warn("AllowDespawn event with no entity");
+                return;
+            }
+            BedtimeBanMod.logger.info("Entity being forced to despawn: " + entity);
+        }
     }
 }
